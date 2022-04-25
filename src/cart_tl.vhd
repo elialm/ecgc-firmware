2b5b7cc7cb405ae403fde08b395b488e730d2ba6
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2022-04-25 16:55:29 +0200

    First working prototype!

--- src/cart_tl.vhd
+++ src/cart_tl.vhd
@@ -28,6 +28,7 @@ use MACHXO3D.all;
 
 entity cart_tl is
     port (
+		-- Gameboy signals
         GB_CLK      : in std_logic;
         GB_ADDR     : in std_logic_vector(15 downto 0);
         GB_DATA     : inout std_logic_vector(7 downto 0);
@@ -35,8 +36,17 @@ entity cart_tl is
         GB_RDN      : in std_logic;
 		GB_CSN      : in std_logic;
 		
+		-- Bus tranceivers
+		BTA_OEN		: out std_logic;
+		BTD_OEN		: out std_logic;
+		BTD_DIR		: out std_logic;
+
 		-- Temporary for testing
-		USER_RST	: in std_logic);
+		USER_RST	: in std_logic;
+		LED_RST		: out std_logic;
+		LED_GB_CLK	: out std_logic;
+		LED_WB_CLK	: out std_logic;
+		LED_OFF		: out std_logic_vector(4 downto 0));
 end cart_tl;
 
 architecture behaviour of cart_tl is
@@ -99,6 +109,9 @@ architecture behaviour of cart_tl is
 	signal rom_cyc : std_logic;
 	signal rom_ack : std_logic;
 
+	signal led_gb_clk_divider : std_logic_vector(18 downto 0);
+	signal led_wb_clk_divider : std_logic_vector(24 downto 0);
+
 begin
 
     GB_SIGNAL_DECODER : component gb_decoder
@@ -110,7 +123,7 @@ begin
         GB_DATA_OUT => gb_data_outgoing,
 		GB_RDN => GB_RDN,
 		GB_CSN => GB_CSN,
-		CLK_I => WB_CLK_I,
+		CLK_I => wb_clk_i,
 		STB_O => rom_stb,
 		CYC_O => rom_cyc,
         ADR_O => wb_adr_o,
@@ -120,10 +133,21 @@ begin
 		
     GB_DATA <= gb_data_outgoing when (GB_CLK nor GB_RDN) = '1' else "ZZZZZZZZ";
     gb_data_incoming <= GB_DATA;
+
+	-- LED indicator for reset state [TEMP]
+	LED_RST <= not(USER_RST);
+
+	-- Other leds off [TEMP]
+	LED_OFF <= (others => '1');
+	
+	-- Bus tranceiver control [TEMP]
+	BTA_OEN <= USER_RST;
+	BTD_OEN <= GB_CLK or USER_RST;
+	BTD_DIR <= '0';
 	
 	CARTRIDGE_MEMORY_CONTROLLER : component cmc
 	port map (
-		CLK_I => WB_CLK_I,
+		CLK_I => wb_clk_i,
 		RST_I => USER_RST,
 		STB_I => rom_stb,
 		CYC_I => rom_cyc,
@@ -142,5 +166,31 @@ begin
 		OSC => wb_clk_i,
 		SEDSTDBY => open,
 		OSCESB => open);
+
+	process (GB_CLK)
+	begin
+		if rising_edge(GB_CLK) then
+			if USER_RST = '1' then
+				led_gb_clk_divider <= (others => '0');
+			else
+				led_gb_clk_divider <= std_logic_vector(unsigned(led_gb_clk_divider) + 1);
+			end if;
+		end if;
+	end process;
+
+	LED_GB_CLK <= not(led_gb_clk_divider(led_gb_clk_divider'high));
+
+	process (wb_clk_i)
+	begin
+		if rising_edge(wb_clk_i) then
+			if USER_RST = '1' then
+				led_wb_clk_divider <= (others => '0');
+			else
+				led_wb_clk_divider <= std_logic_vector(unsigned(led_wb_clk_divider) + 1);
+			end if;
+		end if;
+	end process;
+
+	LED_WB_CLK <= not(led_wb_clk_divider(led_wb_clk_divider'high));
     
 end behaviour;
