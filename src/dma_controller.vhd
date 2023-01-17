0e162ca52b6702ff1155b7a97677b5d6b6476361
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-01-17 15:06:57 +0100

    Finished with simple DMA controller

--- src/dma_controller.vhd
+++ src/dma_controller.vhd
@@ -20,6 +20,8 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_misc.all;
 
 entity dma_controller is
     port (
@@ -38,51 +40,172 @@ entity dma_controller is
         CFG_CYC_I   : in std_logic;
         CFG_ACK_O   : out std_logic;
         CFG_WE_I    : in std_logic;
-        CFG_ADR_I   : in std_logic_vector(7 downto 0);
+        CFG_ADR_I   : in std_logic_vector(3 downto 0);
         CFG_DAT_O   : out std_logic_vector(7 downto 0);
         CFG_DAT_I   : in std_logic_vector(7 downto 0));
 end dma_controller;
 
 architecture behaviour of dma_controller is
 
-    signal master_address   : std_logic_vector(15 downto 0);
+    type DMA_STATE_TYPE is (DMAS_IDLE, DMAS_READ, DMAS_WRITE);
+
+    signal dma_current_state    : DMA_STATE_TYPE;
+    signal dma_addr_src_inc     : std_logic;
+    signal dma_addr_dest_inc    : std_logic;
+    signal dma_copy_amount      : std_logic_vector(7 downto 0);
+    signal dma_amount_is_zero   : std_logic;
+    signal dma_start            : std_logic;
+    signal dma_is_busy          : std_logic;
+
+    signal master_addr_sel  : std_logic;
+    signal master_addr_src  : std_logic_vector(15 downto 0);
+    signal master_addr_dest : std_logic_vector(15 downto 0);
     signal master_data      : std_logic_vector(7 downto 0);
+
     signal slave_data       : std_logic_vector(7 downto 0);
+    signal slave_ack        : std_logic;
 
 begin
 
-    -- DMA state machine
+    dma_amount_is_zero <= nor_reduce(dma_copy_amount);
+    dma_is_busy <= '0' when dma_current_state = DMAS_IDLE else '1';
+
+    -- DMA_ADR_O control
+    with master_addr_sel select DMA_ADR_O <=
+        master_addr_src     when '0',
+        master_addr_dest    when others;
+
     process (CLK_I)
     begin
         if rising_edge(CLK_I) then
+            slave_ack <= '0';
+
             if RST_I = '1' then
-                master_address <= (others => '0');
+                dma_current_state <= DMAS_IDLE;
+                dma_addr_src_inc <= '0';
+                dma_addr_dest_inc <= '0';
+                dma_copy_amount <= (others => '0');
+                dma_start <= '0';
+
+                master_addr_sel <= '0';
+                master_addr_src <= (others => '0');
+                master_addr_dest <= (others => '0');
                 master_data <= (others => '0');
-            
+
+                slave_data <= (others => '0');
+
                 DMA_CYC_O <= '0';
                 DMA_WE_O <= '0';
             else
+                -- DMA state machine
+                case dma_current_state is
+                    when DMAS_IDLE =>
+                        if dma_start = '1' then
+                            dma_start <= '0';
+                            -- master_addr_sel <= '0';
+                            DMA_CYC_O <= '1';
+
+                            dma_current_state <= DMAS_READ;
+                        end if;
+                    when DMAS_READ =>
+                        if DMA_ACK_I = '1' then
+                            dma_copy_amount <= std_logic_vector(unsigned(dma_copy_amount) - 1);
+                            master_addr_sel <= '1';
+                            master_data <= DMA_DAT_I;
+
+                            if dma_addr_src_inc = '1' then
+                                master_addr_src <= std_logic_vector(unsigned(master_addr_src) + 1);
+                            end if;
+
+                            DMA_WE_O <= '1';
+
+                            dma_current_state <= DMAS_WRITE;
+                        end if;
+                    when DMAS_WRITE =>
+                        if DMA_ACK_I = '1' then
+                            master_addr_sel <= '0';
+                            DMA_WE_O <= '0';
+
+                            if dma_addr_dest_inc = '1' then
+                                master_addr_dest <= std_logic_vector(unsigned(master_addr_dest) + 1);
+                            end if;
+
+                            if dma_amount_is_zero = '1' then
+                                DMA_CYC_O <= '0';
+                                dma_current_state <= DMAS_IDLE;
+                            else
+                                master_addr_sel <= '0';
+                                dma_current_state <= DMAS_READ;
+                            end if;
+                        end if;
+                end case;
+
+                -- Configuration address decoder
+                if (CFG_CYC_I and not(slave_ack)) = '1' then
+                    case CFG_ADR_I is
+                        -- Configuration register
+                        when b"0000" =>
+                            if CFG_WE_I = '1' then
+                                dma_start <= CFG_DAT_I(7);
+                                dma_addr_src_inc <= CFG_DAT_I(4);
+                                dma_addr_dest_inc <= CFG_DAT_I(5);
+                            else
+                                slave_data <= dma_start & "0" & dma_addr_dest_inc & dma_addr_src_inc & "000" & dma_is_busy;
+                            end if;
+
+                        -- Copy amount
+                        when b"0001" =>
+                            if CFG_WE_I = '1' then
+                                dma_copy_amount <= CFG_DAT_I;
+                            else
+                                slave_data <= dma_copy_amount;
+                            end if;
+                        
+                        -- Source address low
+                        when b"0100" =>
+                            if CFG_WE_I = '1' then
+                                master_addr_src(7 downto 0) <= CFG_DAT_I;
+                            else
+                                slave_data <= master_addr_src(7 downto 0);
+                            end if;
+
+                        -- Source address high
+                        when b"0101" =>
+                            if CFG_WE_I = '1' then
+                                master_addr_src(15 downto 8) <= CFG_DAT_I;
+                            else
+                                slave_data <= master_addr_src(15 downto 8);
+                            end if;
+
+                        -- Destination address low
+                        when b"0110" =>
+                            if CFG_WE_I = '1' then
+                                master_addr_dest(7 downto 0) <= CFG_DAT_I;
+                            else
+                                slave_data <= master_addr_dest(7 downto 0);
+                            end if;
+
+                        -- Destination address high
+                        when b"0111" =>
+                            if CFG_WE_I = '1' then
+                                master_addr_dest(15 downto 8) <= CFG_DAT_I;
+                            else
+                                slave_data <= master_addr_dest(15 downto 8);
+                            end if;
+
+                        when others =>
+                            slave_data <= x"00";
+                    end case;
+
+                    slave_ack <= '1';
+                end if;
             end if;
         end if;
     end process;
 
-    DMA_ADR_O <= master_address;
     DMA_DAT_O <= master_data;
 
-    -- Configuration address decoder
-    process (CLK_I)
-    begin
-        if rising_edge(CLK_I) then
-            CFG_ACK_O <= '0';
-
-            if RST_I = '1' then
-                slave_data <= (others => '0');
-            elsif CFG_CYC_I = '1' then
-                -- TODO
-            end if;
-        end if;
-    end process;
-
     CFG_DAT_O <= slave_data;
+    CFG_ACK_O <= slave_ack;
 
 end behaviour;
--- src/sim/dma_controller_tb.vhd
+++ src/sim/dma_controller_tb.vhd
@@ -21,6 +21,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_misc.all;
+use ieee.math_real.all;
 
 entity dma_controller_tb is
 end dma_controller_tb;
@@ -40,11 +41,26 @@ architecture behaviour of dma_controller_tb is
     signal cfg_cyc_i    : std_logic := '0';
     signal cfg_ack_o    : std_logic;
     signal cfg_we_i     : std_logic := '0';
-    signal cfg_adr_i    : std_logic_vector(7 downto 0) := (others => '0');
+    signal cfg_adr_i    : std_logic_vector(3 downto 0) := (others => '0');
     signal cfg_dat_o    : std_logic_vector(7 downto 0);
     signal cfg_dat_i    : std_logic_vector(7 downto 0) := (others => '0');
 
-    signal transaction_id : natural := 0;
+    signal transaction_id   : natural := 0;
+    
+    shared variable random_seed1   : positive := 467832;
+    shared variable random_seed2   : positive := 785342;
+
+    impure function rand_vec(len : integer) return std_logic_vector is
+        variable r              : real;
+        variable vec            : std_logic_vector(len - 1 downto 0);
+    begin
+        for i in vec'range loop
+            uniform(random_seed1, random_seed2, r);
+            vec(i) := '1' when r > 0.5 else '0';
+        end loop;
+
+        return vec;
+    end function;
 
 begin
 
@@ -74,6 +90,22 @@ begin
         end loop;
     end process;
 
+    -- Present random data to DMA
+    process (clk_i)
+    begin
+        if rising_edge(clk_i) then
+            dma_ack_i <= '0';
+
+            if (dma_cyc_o and not(dma_ack_i)) = '1' then
+                if dma_we_o = '0' then
+                    dma_dat_i <= rand_vec(8);
+                end if;
+
+                dma_ack_i <= '1';
+            end if;
+        end if;
+    end process;
+
     -- Bus transactions
     process (clk_i)
     begin
@@ -85,6 +117,65 @@ begin
                     null;
                 when 1 =>
                     rst_i <= '0';
+                when 2 =>
+                    cfg_cyc_i <= '1';
+                    cfg_we_i <= '1';
+                    cfg_adr_i <= x"5";
+                    cfg_dat_i <= x"A0";
+                when 3 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 4 =>
+                    cfg_adr_i <= x"4";
+                    cfg_dat_i <= x"5B";
+                when 5 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 6 =>
+                    cfg_adr_i <= x"7";
+                    cfg_dat_i <= x"40";
+                when 7 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 8 =>
+                    cfg_adr_i <= x"6";
+                    cfg_dat_i <= x"00";
+                when 9 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 10 =>
+                    cfg_adr_i <= x"1";
+                    cfg_dat_i <= b"0000_1000";
+                when 11 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 12 =>
+                    cfg_adr_i <= x"0";
+                    cfg_dat_i <= b"1010_0000";
+                when 13 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 14 =>
+                    cfg_cyc_i <= '0';
+                    cfg_we_i <= '0';
+                when 15 =>
+                    null;
+                when 16 =>
+                    null;
+                when 17 =>
+                    null;
+                when 18 =>
+                    null;
+                when 19 =>
+                    null;
+                when 20 =>
+                    cfg_cyc_i <= '1';
+                    cfg_adr_i <= x"0";
+                when 21 =>
+                    -- slave ack is asserted this cycle
+                    null;
+                when 22 =>
+                    cfg_cyc_i <= '0';
                 when others =>
                     transaction_id <= transaction_id;
             end case;
