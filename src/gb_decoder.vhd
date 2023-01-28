88c67294a87561c5c93d6c565e1be795644383c1
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-01-28 14:56:32 +0100

    Rewrote gb_decoder to fix broken mbch

--- src/gb_decoder.vhd
+++ src/gb_decoder.vhd
@@ -51,7 +51,7 @@ end gb_decoder;
 
 architecture behaviour of gb_decoder is
 
-    type WB_STATE_TYPE is (WBS_AWAIT_RISING_CLK, WBS_IDLE, WBS_AWAIT_GB_WRITE, WBS_AWAIT_SLAVE, WBS_AWAIT_FALLING_CLK);
+    type GAMEBOY_BUS_STATE_TYPE is (GBBS_AWAIT_ACCESS_FINISHED, GBBS_IDLE, GBBS_READ_AWAIT_ACK, GBBS_WRITE_AWAIT_FALLING_EDGE, GBBS_WRITE_AWAIT_ACK);
 
     component synchroniser is
     generic (
@@ -65,23 +65,17 @@ architecture behaviour of gb_decoder is
         DAT_OUT : out std_logic_vector(DATA_WIDTH-1 downto 0));
     end component;
     
-    -- Output register for latching read data from WishBone
-    signal data_read_register : std_logic_vector(7 downto 0);
-    
     -- Synchronised signals from GameBoy
     signal gb_clk_sync : std_logic;
     signal gb_csn_sync : std_logic;
     signal gb_addr_sync : std_logic_vector(2 downto 0);
-    signal gb_data_sync : std_logic_vector(7 downto 0);
     
     -- Access signals (comnbinatorial)
     signal gb_access_rom 	: std_logic;
     signal gb_access_ram 	: std_logic;
     signal gb_access_cart	: std_logic;
-    
-    -- WishBone signals
-    signal wb_state : WB_STATE_TYPE;
-    signal wb_cyc   : std_logic;
+
+    signal gb_bus_state     : GAMEBOY_BUS_STATE_TYPE;
 
     signal cyc_counter	: std_logic_vector(3 downto 0);
     signal cyc_timeout	: std_logic;
@@ -110,12 +104,13 @@ begin
         RST => RST_I,
         DAT_IN(0) => GB_CSN,
         DAT_OUT(0) => gb_csn_sync);
-        
-    GB_DATA_OUT <= data_read_register;
     
     -- Signals for determining type of access
-    gb_access_rom <= not(gb_addr_sync(2));												-- A15
-    gb_access_ram <= not(gb_csn_sync) and not(gb_addr_sync(1)) and gb_addr_sync(0);		-- ... not(A14) and A13;
+    gb_access_rom <= not(gb_addr_sync(2));      -- 0x0000 - 0x7FFF
+    gb_access_ram <= not(gb_csn_sync)
+        and gb_addr_sync(2)
+        and not(gb_addr_sync(1))
+        and gb_addr_sync(0);                    -- 0xA000 - 0xBFFF
     gb_access_cart <= gb_access_rom or gb_access_ram;
 
     ACCESS_ROM <= gb_access_rom;
@@ -126,54 +121,55 @@ begin
     begin
         if rising_edge(CLK_I) then
             if RST_I = '1' then
-                data_read_register <= "00000000";
-                wb_state <= WBS_AWAIT_RISING_CLK;
-                wb_cyc <= '0';
-                cyc_counter <= (others => '0');
+                gb_bus_state <= GBBS_AWAIT_ACCESS_FINISHED;
+
+                CYC_O <= '0';
                 WE_O <= '0';
+                ADR_O <= (others => '0');
+                DAT_O <= (others => '0');
+                GB_DATA_OUT <= (others => '0');
                 WB_TIMEOUT <= '0';
             else
-                -- NOTE: WBS_AWAIT_RISING_CLK and WBS_IDLE could be implemented into 1 state... probably...
-                case wb_state is
-                    when WBS_AWAIT_RISING_CLK =>
-                        if gb_clk_sync = '1' then
-                            wb_state <= WBS_IDLE;
+                case gb_bus_state is
+                    when GBBS_AWAIT_ACCESS_FINISHED =>
+                        if gb_access_cart = '0' then
+                            gb_bus_state <= GBBS_IDLE;
                         end if;
-                    when WBS_IDLE =>
+                    when GBBS_IDLE =>
                         if gb_access_cart = '1' then
                             if GB_RDN = '0' then
-                                -- Reading from cart
-                                wb_cyc <= '1';
-                                wb_state <= WBS_AWAIT_SLAVE;
-                                cyc_counter <= "1001";
+                                -- Initiate read from cart
+                                gb_bus_state <= GBBS_READ_AWAIT_ACK;
+                                CYC_O <= '1';
+                                WE_O <= '0';
                             else
-                                -- Writing to cart
-                                wb_state <= WBS_AWAIT_GB_WRITE;
+                                -- Initiate write to cart
+                                gb_bus_state <= GBBS_WRITE_AWAIT_FALLING_EDGE;
                             end if;
+
+                            ADR_O <= GB_ADDR;
                         end if;
-                    when WBS_AWAIT_GB_WRITE =>
+                    when GBBS_READ_AWAIT_ACK =>
+                        if ACK_I = '1' then
+                            gb_bus_state <= GBBS_AWAIT_ACCESS_FINISHED;
+                            CYC_O <= '0';
+                            GB_DATA_OUT <= DAT_I;
+                        end if;
+                    when GBBS_WRITE_AWAIT_FALLING_EDGE =>
                         if gb_clk_sync = '0' then
-                            -- Start write on falling edge
-                            wb_cyc <= '1';
+                            gb_bus_state <= GBBS_WRITE_AWAIT_ACK;
+                            CYC_O <= '1';
                             WE_O <= '1';
                             DAT_O <= GB_DATA_IN;
-                            wb_state <= WBS_AWAIT_SLAVE;
-                            cyc_counter <= "1001";
                         end if;
-                    when WBS_AWAIT_SLAVE =>
+                    when GBBS_WRITE_AWAIT_ACK =>
                         if ACK_I = '1' then
-                            wb_cyc <= '0';
-                            WE_O <= '0';
-                            data_read_register <= DAT_I;
-                            wb_state <= WBS_AWAIT_FALLING_CLK;
-                        elsif cyc_timeout = '1' then
-                            -- WishBone timeout occurred							
-                            WB_TIMEOUT <= '1';
+                            gb_bus_state <= GBBS_AWAIT_ACCESS_FINISHED;
+                            CYC_O <= '0';
                         end if;
-                    when WBS_AWAIT_FALLING_CLK =>
-                        if gb_clk_sync = '0' then
-                            wb_state <= WBS_AWAIT_RISING_CLK;
-                        end if;
+                    when others =>
+                        gb_bus_state <= GBBS_AWAIT_ACCESS_FINISHED;
+                        CYC_O <= '0';
                 end case;
             end if;
 
@@ -189,13 +185,4 @@ begin
     else generate
         cyc_timeout <= '0';
     end generate;
-
-    CYC_O <= wb_cyc;
-    
-    -- TODO: test whether it is really necessary to only output when
-    --			in a cycle. The slave should ignore it otherwise
-    --			anyways. GB_ADDR being asychronous to CLK_I could 
-    --			introduce wierd glitches, who knows...
-    ADR_O <= GB_ADDR when wb_cyc = '1' else x"0000";
-    
 end behaviour;
