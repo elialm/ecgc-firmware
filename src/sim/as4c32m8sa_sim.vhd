fa9ba7d970330de9048351f26564d755d592bbe1
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-02-07 15:28:57 +0100

    Changed all caps libraries to lower caps

--- src/sim/as4c32m8sa_sim.vhd
+++ src/sim/as4c32m8sa_sim.vhd
@@ -26,10 +26,10 @@
 --      - add mechanism for setting mode register
 --      - add actual memory
 
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-use IEEE.std_logic_textio.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_textio.all;
 
 library STD;
 use STD.textio.all;
