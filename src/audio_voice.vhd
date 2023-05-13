559f203a419690f0379e1b00bdee64ef576a789f
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-05-13 15:39:45 +0200

    Added counter top vol control

--- src/audio_voice.vhd
+++ src/audio_voice.vhd
@@ -25,7 +25,7 @@ entity audio_voice is
         CLK_S   : in std_logic;     -- Sample clock
         RST_I   : in std_logic;
 
-        -- Ports for connecting to a sample_table
+        -- Ports for connecting to a sample_table and audio control
         SMPL_EN     : out std_logic;
         SMPL_A      : out std_logic_vector(7 downto 0);
         SMPL_D      : in std_logic_vector(7 downto 0);
@@ -39,8 +39,7 @@ architecture behaviour of audio_voice is
 
     constant TRIANGLE_INIT          : std_logic_vector(9 downto 0) := "0000000000";
     constant TRIANGLE_BOTTOM        : std_logic_vector(9 downto 0) := "0000000010";  -- Dec = 2
-    constant TRIANGLE_TOP           : std_logic_vector(9 downto 0) := "0100011110";  -- Dec = 286
-    -- constant TRIANGLE_TOP           : std_logic_vector(9 downto 0) := "1000011110";  -- Dec = 542
+    constant TRIANGLE_TOP_MIN       : std_logic_vector(9 downto 0) := "0100011110";  -- Dec = 286
     constant SAMPLE_DIVIDER_INIT    : std_logic_vector(10 downto 0) := "00000011110"; -- f_audio ~= 4kHz
 
     signal sample_index         : std_logic_vector(7 downto 0);
@@ -49,6 +48,7 @@ architecture behaviour of audio_voice is
     signal sample_offset        : std_logic_vector(8 downto 0);
 
     signal triangle_counter     : std_logic_vector(9 downto 0);
+    signal triangle_top         : std_logic_vector(9 downto 0);
     signal triangle_upcounting  : std_logic;
     signal triangle_is_top      : std_logic;
     signal triangle_is_bottom   : std_logic;
@@ -66,6 +66,7 @@ begin
         if rising_edge(CLK_I) then
             if RST_I = '1' then
                 triangle_counter <= TRIANGLE_INIT;
+                triangle_top <= TRIANGLE_TOP_MIN;
                 triangle_upcounting <= '1';
             else
                 -- Increment/decrement triangle wave counter
@@ -77,6 +78,7 @@ begin
 
                 if triangle_is_bottom = '1' then
                     triangle_upcounting <= '1';
+                    triangle_top <= std_logic_vector(unsigned(TRIANGLE_TOP_MIN) + unsigned(not(SMPL_VOL(1 downto 0)) & "000000"));
                 end if;
 
                 if triangle_is_top = '1' then
