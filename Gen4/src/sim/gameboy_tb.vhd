d43f3d80a2ff26c8c1cf4e81001f01303fd85671
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2024-01-05 12:33:45 +0100

    Converted naming convention

--- Gen4/src/sim/gameboy_tb.vhd
+++ Gen4/src/sim/gameboy_tb.vhd
@@ -29,76 +29,76 @@ architecture rtl of gameboy_tb is
 
     component cart_tl
         generic (
-            SIMULATION : boolean := TRUE
+            p_simulation : boolean := TRUE
         );
         port (
-            FPGA_CLK33M        : in std_logic;
-            CLK_EN             : out std_logic;
-            FPGA_SOFT_RSTN     : in std_logic;
-            GB_ADDR            : in std_logic_vector(15 downto 0);
-            GB_DATA            : inout std_logic_vector(7 downto 0);
-            GB_BUS_EN          : out std_logic;
-            GB_CLK             : in std_logic;
-            GB_CSN             : in std_logic;
-            GB_RDN             : in std_logic;
-            GB_WRN             : in std_logic;
-            GB_RSTN            : out std_logic;
-            RAM_ADQ            : inout std_logic_vector(15 downto 0);
-            RAM_A              : out std_logic_vector(5 downto 0);
-            RAM_ADVN           : out std_logic;
-            RAM_CE0N           : out std_logic;
-            RAM_CE1N           : out std_logic;
-            RAM_CLK            : out std_logic;
-            RAM_CRE            : out std_logic;
-            RAM_LBN            : out std_logic;
-            RAM_UBN            : out std_logic;
-            RAM_OEN            : out std_logic;
-            RAM_WAIT           : in std_logic;
-            RAM_WEN            : out std_logic;
-            FPGA_SPI_CLK       : inout std_logic;
-            FPGA_SPI_MISO      : inout std_logic;
-            FPGA_SPI_MOSI      : inout std_logic;
-            FPGA_SPI_FLASH_CSN : out std_logic;
-            FPGA_SPI_RTC_CSN   : out std_logic;
-            FPGA_SPI_SD_CSN    : out std_logic;
-            FPGA_USER          : inout std_logic_vector(5 downto 0);
-            RTC_RSTN           : in std_logic;
-            SD_CARD_DETECT     : in std_logic
+            i_fpga_clk33m        : in std_logic;
+            o_clk_en             : out std_logic;
+            i_fpga_rstn     : in std_logic;
+            i_gb_addr            : in std_logic_vector(15 downto 0);
+            io_gb_data            : inout std_logic_vector(7 downto 0);
+            o_gb_bus_en          : out std_logic;
+            i_gb_clk             : in std_logic;
+            i_gb_csn             : in std_logic;
+            i_gb_rdn             : in std_logic;
+            i_gb_wrn             : in std_logic;
+            o_gb_rstn            : out std_logic;
+            io_ram_adq            : inout std_logic_vector(15 downto 0);
+            o_ram_a              : out std_logic_vector(5 downto 0);
+            o_ram_advn           : out std_logic;
+            o_ram_ce0n           : out std_logic;
+            o_ram_ce1n           : out std_logic;
+            o_ram_clk            : out std_logic;
+            o_ram_cre            : out std_logic;
+            o_ram_lbn            : out std_logic;
+            o_ram_ubn            : out std_logic;
+            o_ram_oen            : out std_logic;
+            i_ram_wait           : in std_logic;
+            o_ram_wen            : out std_logic;
+            io_fpga_spi_clk       : inout std_logic;
+            io_fpga_spi_miso      : inout std_logic;
+            io_fpga_spi_miso      : inout std_logic;
+            o_fpga_spi_flash_csn : out std_logic;
+            o_fpga_spi_rtc_csn   : out std_logic;
+            o_fpga_spi_sd_csn    : out std_logic;
+            io_fpga_user          : inout std_logic_vector(5 downto 0);
+            i_rtc_rstn           : in std_logic;
+            i_sd_card_detect     : in std_logic
         );
     end component;
 
-    signal fpga_clk33m : std_logic := '0';
-    signal clk_en : std_logic;
-    signal fpga_soft_rstn : std_logic := '0';
-    signal gb_addr : std_logic_vector(15 downto 0);
-    signal gb_data : std_logic_vector(7 downto 0);
-    signal gb_bus_en : std_logic;
-    signal gb_clk : std_logic := '0';
-    signal gb_csn : std_logic := '1';
-    signal gb_rdn : std_logic := '1';
-    signal gb_wrn : std_logic := '1';
-    signal gb_rstn : std_logic;
-    signal ram_adq : std_logic_vector(15 downto 0);
-    signal ram_a : std_logic_vector(5 downto 0);
-    signal ram_advn : std_logic;
-    signal ram_ce0n : std_logic;
-    signal ram_ce1n : std_logic;
-    signal ram_clk : std_logic;
-    signal ram_cre : std_logic;
-    signal ram_lbn : std_logic;
-    signal ram_ubn : std_logic;
-    signal ram_oen : std_logic;
-    signal ram_wait : std_logic := '0';
-    signal ram_wen : std_logic;
-    signal fpga_spi_clk : std_logic;
-    signal fpga_spi_miso : std_logic;
-    signal fpga_spi_mosi : std_logic;
-    signal fpga_spi_flash_csn : std_logic;
-    signal fpga_spi_rtc_csn : std_logic;
-    signal fpga_spi_sd_csn : std_logic;
-    signal fpga_user : std_logic_vector(5 downto 0);
-    signal rtc_rstn : std_logic := '1';
-    signal sd_card_detect : std_logic := '1';
+    signal n_fpga_clk33m : std_logic := '0';
+    signal n_clk_en : std_logic;
+    signal n_fpga_soft_rstn : std_logic := '0';
+    signal n_gb_addr : std_logic_vector(15 downto 0);
+    signal n_gb_data : std_logic_vector(7 downto 0);
+    signal n_gb_bus_en : std_logic;
+    signal n_gb_clk : std_logic := '0';
+    signal n_gb_csn : std_logic := '1';
+    signal n_gb_rdn : std_logic := '1';
+    signal n_gb_wrn : std_logic := '1';
+    signal n_gb_rstn : std_logic;
+    signal n_ram_adq : std_logic_vector(15 downto 0);
+    signal n_ram_a : std_logic_vector(5 downto 0);
+    signal n_ram_advn : std_logic;
+    signal n_ram_ce0n : std_logic;
+    signal n_ram_ce1n : std_logic;
+    signal n_ram_clk : std_logic;
+    signal n_ram_cre : std_logic;
+    signal n_ram_lbn : std_logic;
+    signal n_ram_ubn : std_logic;
+    signal n_ram_oen : std_logic;
+    signal n_ram_wait : std_logic := '0';
+    signal n_ram_wen : std_logic;
+    signal n_fpga_spi_clk : std_logic;
+    signal n_fpga_spi_miso : std_logic;
+    signal n_fpga_spi_mosi : std_logic;
+    signal n_fpga_spi_flash_csn : std_logic;
+    signal n_fpga_spi_rtc_csn : std_logic;
+    signal n_fpga_spi_sd_csn : std_logic;
+    signal n_fpga_user : std_logic_vector(5 downto 0);
+    signal n_rtc_rstn : std_logic := '1';
+    signal n_sd_card_detect : std_logic := '1';
 
     --Each element is a bitfield
         --0-7       : data (if write)
@@ -144,59 +144,59 @@ begin
 
     inst_cart : cart_tl
     port map(
-        FPGA_CLK33M => fpga_clk33m,
-        CLK_EN => clk_en,
-        FPGA_SOFT_RSTN => fpga_soft_rstn,
-        GB_ADDR => gb_addr,
-        GB_DATA => gb_data,
-        GB_BUS_EN => gb_bus_en,
-        GB_CLK => gb_clk,
-        GB_CSN => gb_csn,
-        GB_RDN => gb_rdn,
-        GB_WRN => gb_wrn,
-        GB_RSTN => gb_rstn,
-        RAM_ADQ => ram_adq,
-        RAM_A => ram_a,
-        RAM_ADVN => ram_advn,
-        RAM_CE0N => ram_ce0n,
-        RAM_CE1N => ram_ce1n,
-        RAM_CLK => ram_clk,
-        RAM_CRE => ram_cre,
-        RAM_LBN => ram_lbn,
-        RAM_UBN => ram_ubn,
-        RAM_OEN => ram_oen,
-        RAM_WAIT => ram_wait,
-        RAM_WEN => ram_wen,
-        FPGA_SPI_CLK => fpga_spi_clk,
-        FPGA_SPI_MISO => fpga_spi_miso,
-        FPGA_SPI_MOSI => fpga_spi_mosi,
-        FPGA_SPI_FLASH_CSN => fpga_spi_flash_csn,
-        FPGA_SPI_RTC_CSN => fpga_spi_rtc_csn,
-        FPGA_SPI_SD_CSN => fpga_spi_sd_csn,
-        FPGA_USER => fpga_user,
-        RTC_RSTN => rtc_rstn,
-        SD_CARD_DETECT => sd_card_detect
+        i_fpga_clk33m => n_fpga_clk33m,
+        o_clk_en => n_clk_en,
+        i_fpga_rstn => n_fpga_soft_rstn,
+        i_gb_addr => n_gb_addr,
+        io_gb_data => n_gb_data,
+        o_gb_bus_en => n_gb_bus_en,
+        i_gb_clk => n_gb_clk,
+        i_gb_csn => n_gb_csn,
+        i_gb_rdn => n_gb_rdn,
+        i_gb_wrn => n_gb_wrn,
+        o_gb_rstn => n_gb_rstn,
+        io_ram_adq => n_ram_adq,
+        o_ram_a => n_ram_a,
+        o_ram_advn => n_ram_advn,
+        o_ram_ce0n => n_ram_ce0n,
+        o_ram_ce1n => n_ram_ce1n,
+        o_ram_clk => n_ram_clk,
+        o_ram_cre => n_ram_cre,
+        o_ram_lbn => n_ram_lbn,
+        o_ram_ubn => n_ram_ubn,
+        o_ram_oen => n_ram_oen,
+        i_ram_wait => n_ram_wait,
+        o_ram_wen => n_ram_wen,
+        io_fpga_spi_clk => n_fpga_spi_clk,
+        io_fpga_spi_miso => n_fpga_spi_miso,
+        io_fpga_spi_miso => n_fpga_spi_mosi,
+        o_fpga_spi_flash_csn => n_fpga_spi_flash_csn,
+        o_fpga_spi_rtc_csn => n_fpga_spi_rtc_csn,
+        o_fpga_spi_sd_csn => n_fpga_spi_sd_csn,
+        io_fpga_user => n_fpga_user,
+        i_rtc_rstn => n_rtc_rstn,
+        i_sd_card_detect => n_sd_card_detect
     );
     
     -- GameBoy simulation
     process
         type bus_state is (BS_CLK_UP_UP, BS_CLK_UP_DOWN, BS_CLK_HIGH_UP, BS_CLK_HIGH_DOWN, BS_CLK_DOWN_UP, BS_CLK_DOWN_DOWN, BS_CLK_LOW_UP, BS_CLK_LOW_DOWN);
     
-        variable current_transaction : std_logic_vector(25 downto 0);
-        variable transaction_address : std_logic_vector(15 downto 0);
-        variable transaction_data : std_logic_vector(7 downto 0);
-        variable transaction_is_idle : boolean;
-        variable transaction_is_read : boolean;
+        variable v_current_transaction : std_logic_vector(25 downto 0);
+        variable v_transaction_address : std_logic_vector(15 downto 0);
+        variable v_transaction_data : std_logic_vector(7 downto 0);
+        variable v_transaction_is_idle : boolean;
+        variable v_transaction_is_read : boolean;
     begin
-        -- Wait a bit for the PLL to lock and GB_RSTN to deassert
+        -- Wait a bit for the PLL to lock and o_gb_rstn to deassert
         wait for 5 us;
     
         for i in test_bus_transactions'low to test_bus_transactions'high loop
-            current_transaction := test_bus_transactions(i);
-            transaction_address := current_transaction(23 downto 8);
-            transaction_data := current_transaction(7 downto 0);
-            transaction_is_idle := current_transaction(25) = '0';
-            transaction_is_read := current_transaction(24) = '0';
+            v_current_transaction := test_bus_transactions(i);
+            v_transaction_address := v_current_transaction(23 downto 8);
+            v_transaction_data := v_current_transaction(7 downto 0);
+            v_transaction_is_idle := v_current_transaction(25) = '0';
+            v_transaction_is_read := v_current_transaction(24) = '0';
             
             for state in bus_state loop
                 if enable_double_speed then
@@ -205,34 +205,34 @@ begin
                     wait for 125 ns; -- Normal speed (DMG)
                 end if;
 
-                if not(transaction_is_idle) then
+                if not(v_transaction_is_idle) then
                     case state is
                         when BS_CLK_UP_UP =>
-                            gb_clk <= '1';
-                            gb_data <= (others => 'U');
-                            gb_addr(14 downto 0) <= (others => 'U');
-                            gb_rdn <= '0';
-                            gb_addr(15) <= '1';
-                            gb_csn <= '1';
+                            n_gb_clk <= '1';
+                            n_gb_data <= (others => 'U');
+                            n_gb_addr(14 downto 0) <= (others => 'U');
+                            n_gb_rdn <= '0';
+                            n_gb_addr(15) <= '1';
+                            n_gb_csn <= '1';
                         when BS_CLK_UP_DOWN =>
-                            gb_addr(14 downto 0) <= transaction_address(14 downto 0);
-                            if not(transaction_is_read) then
-                                gb_rdn <= '1';
+                            n_gb_addr(14 downto 0) <= v_transaction_address(14 downto 0);
+                            if not(v_transaction_is_read) then
+                                n_gb_rdn <= '1';
                             end if;
                         when BS_CLK_HIGH_UP =>
-                            gb_addr(15) <= transaction_address(15);
-                            gb_csn <= not(transaction_address(15));
+                            n_gb_addr(15) <= v_transaction_address(15);
+                            n_gb_csn <= not(v_transaction_address(15));
                             -- Data should be presented during read
-                            if transaction_is_read then
-                                gb_data <= (others => 'Z');
+                            if v_transaction_is_read then
+                                n_gb_data <= (others => 'Z');
                             end if;
                         when BS_CLK_HIGH_DOWN =>
                             null;
                         when BS_CLK_DOWN_UP =>
-                            gb_clk <= '0';
-                            if not(transaction_is_read) then
-                                gb_wrn <= '0';
-                                gb_data <= transaction_data;
+                            n_gb_clk <= '0';
+                            if not(v_transaction_is_read) then
+                                n_gb_wrn <= '0';
+                                n_gb_data <= v_transaction_data;
                             end if;
                         when BS_CLK_DOWN_DOWN =>
                             null;
@@ -240,21 +240,21 @@ begin
                             -- Data is sampled by the Gameboy (I think?)
                             null;
                         when BS_CLK_LOW_DOWN =>
-                            gb_wrn <= '1';
+                            n_gb_wrn <= '1';
                     end case;
                 else
-                    gb_addr(14 downto 0) <= (others => 'U');
-                    gb_addr(15) <= '1';
-                    gb_csn <= '1';
-                    gb_data <= (others => 'U');
-                    gb_rdn <= '0';
-                    gb_wrn <= '1';
+                    n_gb_addr(14 downto 0) <= (others => 'U');
+                    n_gb_addr(15) <= '1';
+                    n_gb_csn <= '1';
+                    n_gb_data <= (others => 'U');
+                    n_gb_rdn <= '0';
+                    n_gb_wrn <= '1';
                     
                     case state is
                         when BS_CLK_UP_UP =>
-                            gb_clk <= '1';
+                            n_gb_clk <= '1';
                         when BS_CLK_DOWN_UP =>
-                            gb_clk <= '0';
+                            n_gb_clk <= '0';
                         when others =>
                             null;
                     end case;
